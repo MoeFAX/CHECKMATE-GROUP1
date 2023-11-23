9e0d00074e854f893a5ad391dab703f69111b681
Mark Calina <maccalina123@gmail.com>
2023-11-23 12:32:33 +0800

    firebase connection added

--- app/src/main/java/com/example/checkmateapp_grp1/MainActivity.kt
+++ app/src/main/java/com/example/checkmateapp_grp1/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.checkmateapp_grp1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var databaseReference: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //databaseReference = FirebaseDatabase.getInstance().getReference("Type name of database here")
+        
     }
 }
